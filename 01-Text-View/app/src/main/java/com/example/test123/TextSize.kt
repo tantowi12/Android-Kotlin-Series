a86c79c7591963a554dba88c73af5f2c57cf3bad
Tantowi <tantowi@pop-os.localdomain>
2020-06-10 02:16:36 +0700

    Text View

---
+++ 01-Text-View/app/src/main/java/com/example/test123/TextSize.kt
@@ -0,0 +1,11 @@
+package com.example.test123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextSize : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_size)
+    }
+}
